83ed060a05979bb34a4c0afcc9528ed4b4077d9a
Rostradamus <hyungro@hotmail.com>
2017-11-28 13:02:57 -0800

    first commit

---
+++ Tip Calculator/Controllers/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tip Calculator
+//
+//  Created by Ro Lee on 2017-11-28.
+//  Copyright © 2017 Ro Lee. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tip Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tip Calculator
+//
+//  Created by Ro Lee on 2017-11-28.
+//  Copyright © 2017 Ro Lee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
